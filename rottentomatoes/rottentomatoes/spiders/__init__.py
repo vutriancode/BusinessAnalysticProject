c68fc3a003917891521a8574aa1e8f8907b05c08
vutriancode <vutrian172933@gmail.com>
2023-02-11 13:56:34 +0700

    update crawler

--- rottentomatoes/rottentomatoes/spiders/__init__.py
+++ rottentomatoes/rottentomatoes/spiders/__init__.py
@@ -44,6 +44,57 @@ class TomatoesSpider(scrapy.Spider):
         item = response.meta['item']
         item["url"] = response1
         return item
+
+class DetailTomatoesSpider(scrapy.Spider): 
+    name = "DetailTomatoesSpider" 
+    allowed_domains = ["rottentomatoes.com"] 
+    #    files2 = open("tomatoes.txt","a")
+    files2 = open("data1.json","r",encoding="utf-8")
+    a2 = json.load(files2)
+    list_id = [i["imdb_id"] for i in a2]
+    start_urls = [
+        "https://www.rottentomatoes.com"
+    ]
+    def parse(self, response):
+        for i in self.a2[:1]:
+          item = RottentomatoesItem()
+
+          headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.85 Safari/537.36'}
+
+
+          request =  scrapy.Request(i["url"], callback = self.parse_dir_contents,headers = headers)
+          request.meta['item'] = item
+
+          yield request
+    def parse_dir_contents(self, response):
+        #audiencescore
+        audiencescore = response.xpath('//*[@id="topSection"]/div[1]/score-board/@audiencescore').extract()[0]
+        #tomatometerscore
+        tomatometerscore = response.xpath('//*[@id="topSection"]/div[1]/score-board/@tomatometerscore').extract()[0]
+        
+        #get information
+        for li in response.css('li[class="meta-row clearfix"]'):
+            label = li.xpath('div[1]/text()').extract()[0]
+            if label =="Genre:":
+                print(li.xpath('div[2]/text()').extract()[0])
+            if label =="Original Language:":
+                print(li.xpath('div[2]/text()').extract()[0])
+            if label =="Release Date (Theaters):":
+                print(li.xpath('div[2]/time/@datetime').extract()[0])
+            if label == "Release Date (Streaming):":
+                print(li.xpath('div[2]/time/@datetime').extract()[0])
+
+
+            
+        
+
+        
+        #get actor
+        for li in response.css('div[data-qa="cast-crew-item"]'):
+          print(li.xpath('div[2]/a/@href').extract()[0])
+          print(li.xpath('div[2]/a/span/text()').extract()[0].replace("\n",""))
+        item = response.meta['item']
+        return item
 # import requests
 # from bs4 import BeautifulSoup
 # files = open("imdb.json","r",encoding="utf-8")
